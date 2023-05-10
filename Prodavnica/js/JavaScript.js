9564d3588622144c9c86c476803934c216030a54
KristoferJoca <132840555+KristoferJoca@users.noreply.github.com>
2023-05-10 16:01:23 +0200

    Add files via upload

---
+++ Prodavnica/js/JavaScript.js
@@ -0,0 +1,43 @@
+﻿
+//Dugme email
+function text() {
+    alert('Hvala vam na prijavi, Obavestićemo vas o našim novim proizvodima');
+}
+//dugme naruci
+function text11() {
+    alert('Igra uspešno naručena');
+}
+
+
+
+//Dugme za kupon
+function text1() {
+    alert('Šifra vašeg kupona je ASDASF');
+}
+
+//Dugme za Kontakt
+function text2() {
+    alert('Vaša poruka je uspesno poslata');
+}
+//Pretraga
+function myFunction() {
+    // Deklarisanje promenljivih
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('search-item');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // Loop kroz sve na listi, i sakri ono sto se ne poklapa u pretrazi
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("h2")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+
---
+++ Prodavnica/js/JavaScriptproizvod.js
@@ -0,0 +1,50 @@
+﻿
+//Checkbox nestajanje
+
+function checkme() {
+    var cb = document.getElementById("chk");
+    var text = document.getElementById("kup");
+    
+
+    if (cb.checked == true) {
+        text.style.display = "block";
+
+
+    } else {
+        text.style.display = "none";
+    }
+}
+function checkme1() {
+    var cb = document.getElementById("chk");
+    var text = document.getElementById("B");
+
+
+    if (cb.checked == true) {
+        text.style.display = "block";
+
+
+    } else {
+        text.style.display = "none";
+    }
+}
+
+function popust() {
+    var b = document.getElementById("B");
+    
+    const h2 = document.querySelector("#header2");
+
+  
+    h2.innerText = "7.600,00 RSD";
+  
+
+}
+
+
+function popust1() {
+    var b = document.getElementById("B");
+
+    const h2 = document.querySelector("#header2");
+
+
+    h2.innerText = "7.200,00 RSD";
+}
